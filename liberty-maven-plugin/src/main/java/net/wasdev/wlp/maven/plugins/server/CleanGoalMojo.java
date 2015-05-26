d383ff04b8705f60278f215fcbd1ed085a298335
TonyRS <joantors@mx1.ibm.com>
2015-05-26 13:00:36 -0500

    This is just a example goal in maven

---
+++ liberty-maven-plugin/src/main/java/net/wasdev/wlp/maven/plugins/server/CleanGoalMojo.java
@@ -0,0 +1,12 @@
+package net.wasdev.wlp.maven.plugins.server;
+
+import net.wasdev.wlp.maven.plugins.BasicSupport;
+
+public class CleanGoalMojo extends BasicSupport{
+    
+    protected void doExecute() throws Exception {
+       
+        System.out.println("Hello world!!!");
+    }
+    
+}
